27b2f1f9a27031f6c348e31259496c228e7033bd
KyungminLee <yesleekm@gmail.com>
2020-05-24 04:40:55 +0900

    20200524 0440

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
-20/5/23 2200 
+20/5/24 0400 
 
-Web server - printing frames
+Web server 
 ============================
 
 HGU 20-1 capstone project
 ----------------------------
 1. server.js    Main web
 2. /views				
-	* main.ejs    page - root
-	* setting.ejs    page - admin(setting ROI)
-	* waiting.ejs    page - temp
+	* Basic.ejs    page - Main page 
+	* Admin_1.ejs    page - admin(setting resolution and camera's number)
+	* Admin_wait.ejs    page - admin(for waiting) 
+	* Admin_2.ejs	page - admin(setting ROI)
 3. /source    Image source files(after deaplearning process)
 4. /data
 	* people.txt    (IN)data - number of people
 	* camera.txt    (IN)data - IP
-	* ROI.txt    (OUT)data - ROI coordinates value (4xN)
+	* admin_input.txt	(IN)data - resolution/camera_number
+	* mode.txt	(OUT)data - Admin/Basic mode
+	* ROI.txt    	(OUT)data - ROI coordinates value (4xN)
 	* /source_ROI    Image source files(before setting ROI)
 5. /test    test code
 
--- server.js
+++ server.js
@@ -4,16 +4,22 @@ HGU 20-2 capstone web
 */
 
 //1-1) 상태 변수
-var isConnected= false;
-var isAdmin = false;
-//1-2) 지정 변수
+var mode = 1;		// 1~3.admin / 4.basic
+var isSet = false;
+//1-2) 상수
 var express_port = 7070;
 var path_src_deap = 'source/';
 var path_src_ROI = 'data/src_ROI/';
 var path_data_ROI = 'data/ROI.txt';
 var path_data_people = 'data/people.txt';
 var path_data_camera = 'data/camera.txt';
+var path_data_admin = 'data/admin_input.txt'
+var path_data_mode = 'data/mode.txt'
 //1-3) 일반 변수
+//admin-1
+var num_camera;
+var res_capture=new Array(2);;
+var res_resize=new Array(2);
 var client_num = 0;
 var client_log = 0;
 var recent_image_name = "";
@@ -24,7 +30,6 @@ var curr_image_num = -1;
 var setting_bnum = new Array();;
 var setting_plist = new Array();;
 var num_people = -1;
-var num_camera = 0;
 var num_src_deap = new Array();
 var num_src_ROI = 0;
 var num_curr_ROI = 1;
@@ -55,57 +60,111 @@ var fs = require('fs');
 //2-2) 엔진 설정 (view engine - ejs + static path 설정 + express listen)
 app.set("view engine", "ejs");
 app.set('views', __dirname+'/views');
-app.use(express.static(__dirname));	
+app.use(express.static(__dirname));
 app.listen(express_port, function(){
 	console.log('Express Start, Port: ' + express_port);
 });
 
 //3) web root page 설정
 app.get('/', function(req, res){
-	num_camera = fs.readdirSync(path_src_deap).length;
-	for(var i = 0; i<num_camera; i++){
-		list_src_deap[i] = fs.readdirSync(path_src_deap+String(i+1));
-		num_src_deap[i] = list_src_deap[i].length;
-	}
-	list_src_ROI = fs.readdirSync(path_src_ROI);
-	num_src_ROI = list_src_ROI.length;
-	isConnected = (num_src_ROI!=0)? true:false;
 	//3-1) Waiting
-	if(!isConnected){
-		res.render('waiting');
-	}
-	//3-2) Admin mode
-	else if(!isAdmin){
-    res.render('admin', {
-			path: path_src_ROI,
-      img_list: list_src_ROI,
-			num_curr: num_curr_ROI,
-			num_img: num_src_ROI
-    });
-	}
-	//3-3) Basic mode
-	else{
-		list_data_people = fs.readFileSync(path_data_people, 'utf8').toString().split("\n");
-		num_people = list_data_people[0];
-		list_data_camera = fs.readFileSync(path_data_camera, 'utf8').toString().split("\n");
-		res.render('basic', {
-			path: path_src_deap,
-			p_num: num_people,
-			img_list: list_src_deap,
-			c_num: num_camera,
-			c_log: num_src_deap,
-			c_ip: list_data_camera,
-			curr_img_name: curr_image_name,
-			curr_img_camera: curr_image_camera,
-			curr_img_num: curr_image_num,
-			recent_img_name: recent_image_name,
-			recent_img_num: recent_image_num
-		});
+	switch(mode){
+		case 1:
+			res.render('admin_1', {
+				isSet: isSet
+			});
+			break;
+		case 2:
+			list_src_ROI = fs.readdirSync(path_src_ROI);
+			num_src_ROI = list_src_ROI.length;
+			if(!num_src_ROI){
+				res.render('admin_wait');
+			}
+			else{
+				mode = 3;
+				res.redirect('/');
+			}
+			break;
+		case 3:
+			res.render('admin_2', {
+				path: path_src_ROI,
+				img_list: list_src_ROI,
+				num_curr: num_curr_ROI,
+				num_img: num_src_ROI
+			});
+			break;
+		case 4:
+			list_data_people = fs.readFileSync(path_data_people, 'utf8').toString().split("\n");
+			num_people = list_data_people[0];
+			list_data_camera = fs.readFileSync(path_data_camera, 'utf8').toString().split("\n");
+			for(var i = 0; i<num_camera; i++){
+				list_src_deap[i] = fs.readdirSync(path_src_deap+String(i+1));
+				num_src_deap[i] = list_src_deap[i].length;
+			}
+			res.render('basic', {
+				path: path_src_deap,
+				p_num: num_people,
+				img_list: list_src_deap,
+				c_num: num_camera,
+				c_log: num_src_deap,
+				c_ip: list_data_camera,
+				curr_img_name: curr_image_name,
+				curr_img_camera: curr_image_camera,
+				curr_img_num: curr_image_num,
+				recent_img_name: recent_image_name,
+				recent_img_num: recent_image_num
+			});
+			break;
 	}
 });
 
-//4-1) (RESTAPI) basic: filename
-app.get('/image/:camera/:filepath', function(req, res){
+//4) REST API: data using URL
+//4-1) admin_1
+app.get('/ADMIN_1/toADMIN_wait', function(req, res){
+	num_camera=req.query.num_cam;
+	res_capture[0]=req.query.res_cw;
+	res_capture[1]=req.query.res_ch;
+	res_resize[0]=req.query.res_rw;
+	res_resize[1]=req.query.res_rh;
+	mode=2;
+	fs.writeFileSync(path_data_admin, num_camera+"\n"+res_capture[0]+"\n"+res_capture[1]+"\n"+res_resize[0]+"\n"+res_resize[1]+"\n", 'utf8');
+	fs.writeFileSync(path_data_mode, "admin\n", 'utf8');
+	res.redirect('/');
+})
+app.get('/ADMIN_1/toBASIC', function(req, res){
+	console.log("admin_1 to basic");
+	mode = 4;
+	res.redirect('/');
+})
+//4-2) admin_2
+app.get('/ADMIN_2/next', function(req, res){
+	setting_bnum[num_curr_ROI-1] = req.query.b_num;
+	setting_plist[num_curr_ROI-1] = JSON.parse(req.query.p_list);
+	num_curr_ROI++;
+	res.redirect('/');
+});
+app.get('/ADMIN_2/prev', function(req, res){
+	setting_bnum[num_curr_ROI-1] = req.query.b_num;
+	setting_plist[num_curr_ROI-1] = JSON.parse(req.query.p_list);
+	num_curr_ROI--;
+	res.redirect('/');
+});
+app.get('/ADMIN_2/toBASIC', function(req, res){
+	console.log("admin_2 to basic");
+	mode=4;
+	num_curr_ROI = 1;
+	isSet = true;
+	setting_bnum[num_curr_ROI-1] = req.query.b_num;
+	setting_plist[num_curr_ROI-1] = JSON.parse(req.query.p_list);
+	fs.writeFileSync(path_data_ROI, "number of ROI\n"+setting_bnum+"\n\ndata of ROI\n", 'utf8');
+	fs.writeFileSync(path_data_mode, "basic\n", 'utf8');
+	for(var i =0 ; i< num_src_ROI; i++){
+		fs.appendFileSync(path_data_ROI, setting_plist[i] + '\n', 'utf8');
+	}
+	res.redirect('/');
+});
+//4-3) basic
+app.get('/BASIC/image/:camera/:filepath', function(req, res){
 	curr_image_camera = req.params.camera;
 	curr_image_name = req.params.filepath;
 	recent_image_name = list_src_deap[curr_image_camera-1][num_src_deap[curr_image_camera-1]-1];
@@ -113,31 +172,8 @@ app.get('/image/:camera/:filepath', function(req, res){
 	recent_image_num = findNumber(list_data_people , recent_image_name);
 	res.redirect('/');
 });
-//4-2) (RESTAPI) admin: go next image
-app.get('/setting/next', function(req, res){
-	console.log("next");
-	setting_bnum[num_curr_ROI-1] = req.query.b_num;
-	setting_plist[num_curr_ROI-1] = JSON.parse(req.query.p_list);
-	num_curr_ROI++;
+app.get('/BASIC/toADMIN_1', function(req, res){
+	console.log("basic to admin_1");
+	mode = 1;
 	res.redirect('/');
-});
-//4-3) (RESTAPI) admin: go previous image
-app.get('/setting/prev', function(req, res){
-	console.log("prev");
-	setting_bnum[num_curr_ROI-1] = req.query.b_num;
-	setting_plist[num_curr_ROI-1] = JSON.parse(req.query.p_list);
-	num_curr_ROI--;
-	res.redirect('/');
-});
-//4-4) (RESTAPI) admin: finish mode
-app.get('/setting/finish', function(req, res){
-	console.log("finish");
-	setting_bnum[num_curr_ROI-1] = req.query.b_num;
-	setting_plist[num_curr_ROI-1] = JSON.parse(req.query.p_list);
-	isAdmin=true;
-	fs.writeFileSync(path_data_ROI, "number of ROI\n"+setting_bnum+"\n\ndata of ROI\n", 'utf8');
-	for(var i =0 ; i< num_src_ROI; i++){
-		fs.appendFileSync(path_data_ROI, setting_plist[i] + '\n', 'utf8');
-	}
-	res.redirect('/');
-});
+})
